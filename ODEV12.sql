531eba2ef5a3a3f0b3f9e94f223c8f7a3f15947f
Ramazann15 <75622445+Ramazann15@users.noreply.github.com>
2022-03-19 18:24:39 +0300

    Update ODEV12.sql
    SQL

--- ODEV12.sql
+++ ODEV12.sql
@@ -18,9 +18,8 @@ SELECT * FROM film
 WHERE rental_rate =(SELECT MIN(rental_rate)FROM film) AND replacement_cost =(SELECT MIN(replacement_cost) FROM film);
 
 --4) payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
-SELECT first_name ,last_name, amount  FROM customer
+SELECT first_name ,last_name,SUM(amount) FROM customer
 INNER JOIN payment ON payment.customer_id = customer.customer_id
-WHERE  amount = ANY
-(
-	SELECT Max(amount) FROM payment
-)
+GROUP BY  first_name ,last_name
+ORDER BY SUM(amount) DESC
+LIMIT 1
